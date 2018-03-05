ffb24d4f2b085cb4bf926be74e928277289a8823
AliAbbasi <abbasi.ali.tab@gmail.com>
2018-03-05 09:14:56 +0200

    read .csv files

--- utils/json_to_npy.py
+++ utils/json_to_npy.py
@@ -2,7 +2,36 @@
 
 # ----------------------------------------------------------------------------------
 
+import csv
+import glob
+import json
+import sys
 
+# ---------------------------------------------------------------------------------- 
+
+model_category_mapping = {}
+models = {}
+
+# ---------------------------------------------------------------------------------- 
+
+def csv_loader():
+    with open('ModelCategoryMapping.csv') as csvfile:
+        model_category_mapping = csv.DictReader(csvfile)
+        
+    with open('models.csv') as csvfile:
+        models = csv.DictReader(csvfile) 
+        for row in models:
+            print (type(row))
+            for key, value in row.items() :
+                print (key, value)
+            sys.exit(0)
+            print(row['minPoint'])
+
+    
+    
+    print (models )
+    print (models[0] )
+    sys.exit(0)
 # ---------------------------------------------------------------------------------- 
 
 def json_reader(json_file):
@@ -19,6 +48,7 @@ def json_reader(json_file):
             node["id"] # 0_0, 0_1, ...
             node["type"] # Object, Box, Ground, Room
             if node["type"] == "Object":
+                pass
                 # TODO:
                 # get voxelized version of this object from related folder
                 
@@ -26,6 +56,7 @@ def json_reader(json_file):
 
 if __name__ == '__main__':
 
+    csv_loader()
     rooms_of_houses = []
     for json_file in glob.glob('*.json'):
         json_reader(json_file)
