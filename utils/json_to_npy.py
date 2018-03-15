cd5058e797a43ef57a038bfdf25e73ab5c0a984d
AliAbbasi <abbasi.ali.tab@gmail.com>
2018-03-15 17:49:09 +0300

    update todo

--- utils/json_to_npy.py
+++ utils/json_to_npy.py
@@ -77,91 +77,12 @@ def get_room(room, input_json_file):
 
 # ----------------------------------------------------------------------------------
 
-def json_to_npy_with_trans(json_file_input):
-    data = json.load(open(json_file_input))
-    glob_bbox_min = np.full(3, sys.maxint * 1.0)
-    glob_bbox_max = np.full(3, -sys.maxint - 1 * 1.0)
-
-    # to find the bbox_min and bbox_max of all objects
-    for level in data["levels"]:
-        for node in level["nodes"]:
-            if node["type"] == "Object" and node["valid"] == 1:
-                bbox = node["bbox"]
-                bbox_min = np.asarray(bbox["min"])
-                bbox_max = np.asarray(bbox["max"])
-
-                glob_bbox_min[0] = bbox_min[0] if bbox_min[0] < glob_bbox_min[0] else glob_bbox_min[0]
-                glob_bbox_min[1] = bbox_min[1] if bbox_min[1] < glob_bbox_min[1] else glob_bbox_min[1]
-                glob_bbox_min[2] = bbox_min[2] if bbox_min[2] < glob_bbox_min[2] else glob_bbox_min[2]
-
-                glob_bbox_max[0] = bbox_max[0] if bbox_max[0] > glob_bbox_max[0] else glob_bbox_max[0]
-                glob_bbox_max[1] = bbox_max[1] if bbox_max[1] > glob_bbox_max[1] else glob_bbox_max[1]
-                glob_bbox_max[2] = bbox_max[2] if bbox_max[2] > glob_bbox_max[2] else glob_bbox_max[2]
-
-    # determine scene size with respect to the glob_bbox_max - glob_bbox_min
-    scene_size = map(int, ((glob_bbox_max - glob_bbox_min) * 100.0) / 6.0)
-    scene = np.zeros(scene_size)
-
-    # put objects in their places
-    for level in data["levels"]:
-        for node in level["nodes"]:
-            if node["type"] == "Object" and node["valid"] == 1:
-                # fetch the transformation matrix from node["transform"]
-                transformation = np.asarray(node["transform"])
-                # transformation *= 100.0 / 6.0
-                transformation = transformation.reshape(4, 4)
-
-                # find the node["modelId"] (is a string) from current directory
-                str_modelId = str(node["modelId"])
-                object_voxel = np.load(str(node["modelId"] + ".npy"))
-
-                bbox = node["bbox"]
-                bbox_min = np.asarray(bbox["min"])
-                bbox_max = np.asarray(bbox["max"])
-
-                bbox_min -= glob_bbox_min
-                bbox_max -= glob_bbox_min
-                # TODO: care about the negative numbers in bbox
-                bbox_min = map(int, (bbox_min * 100.0) / 6.0)
-                bbox_max = map(int, (bbox_max * 100.0) / 6.0)
-
-                for model in models:
-                    if str(model["id"]) == str_modelId:
-                        aligned_dims = model["aligned.dims"].split(",")
-
-                aligned_dims = np.asarray(aligned_dims, dtype=float)
-                aligned_dims /= 6.0
-                max_dim = np.max(aligned_dims)
-
-                for x in range(int(-max_dim / 2), int(max_dim / 2)):
-                    for y in range(0, int(max_dim)):
-                        for z in range(int(-max_dim / 2), int(max_dim / 2)):
-                            coordinate = np.array([[x], [y], [z], [1]])
-                            new_coordinate = transformation.dot(coordinate)
-                            new_coordinate = map(int, new_coordinate)
-
-                            if object_voxel[x + int(max_dim / 2), y, z + int(max_dim / 2)]:
-                                scene[new_coordinate[0] + object_voxel.shape[0] + bbox_min[0],
-                                      new_coordinate[1] + object_voxel.shape[0] + bbox_min[1],
-                                      new_coordinate[2] + object_voxel.shape[0] + bbox_min[2]] = object_voxel[
-                                    x + int(max_dim / 2), y, z + int(max_dim / 2)]
-
-    np.save(str(json_file_input[:-5]) + ".npy", scene)
-
-
-# ----------------------------------------------------------------------------------
-
-def trans_op(input_object_voxel, input_transformation, input_aligned_dims, input_bbox_min):
-    input_aligned_dims = np.asarray(input_aligned_dims, dtype=float)
-    input_aligned_dims /= 6.0
-    max_dim = np.max(input_aligned_dims)
-    # TODO: reading max_dim from aligned_dims is a mistake, cuz we did read it from bbox_max - bbox_min
-    if int(max_dim) >= input_object_voxel.shape[0]:
-        debugger = 1
-    bound = input_object_voxel.shape[0]
-    new_object_voxel = np.zeros((input_object_voxel.shape[0] + bound,
-                                 input_object_voxel.shape[1] + bound,
-                                 input_object_voxel.shape[2] + bound))
+def trans_op(input_object_voxel, input_transformation):
+    # TODO: there is some staff that cause to have voxels in negative dimension
+    max_dim = np.max(input_object_voxel.shape)
+    new_object_voxel = np.zeros((input_object_voxel.shape[0] + max_dim * 2,
+                                 input_object_voxel.shape[1] + max_dim * 2,
+                                 input_object_voxel.shape[2] + max_dim * 2))
 
     for x in range(int(-max_dim / 2), int(max_dim / 2)):
         for y in range(0, int(max_dim)):
@@ -171,7 +92,8 @@ def trans_op(input_object_voxel, input_transformation, input_aligned_dims, input
                 new_coordinate = np.asarray(map(int, np.around(new_coordinate)))
                 int_max_dim = int(max_dim / 2.0)
                 new_coordinate += int_max_dim
-                # TODO; IndexError: index 84 is out of bounds for axis 1 with size 84
+                # print ("max_dim:", max_dim, " x:", x + int(max_dim/2), " y:", y, " z:", z + int(max_dim/2),
+                #        " new_coordinate:", new_coordinate)
                 new_object_voxel[new_coordinate[0], new_coordinate[1], new_coordinate[2]] = \
                     input_object_voxel[x + int(max_dim/2), y, z + int(max_dim/2)]
 
@@ -187,9 +109,10 @@ def trans_op(input_object_voxel, input_transformation, input_aligned_dims, input
 
     return new_object_voxel
 
+
 # ----------------------------------------------------------------------------------
 
-def json_to_npy_no_trans(json_file_input):
+def json_to_npy(json_file_input):
     data = json.load(open(json_file_input))
     glob_bbox_min = np.full(3, sys.maxint * 1.0)
     glob_bbox_max = np.full(3, -sys.maxint - 1 * 1.0)
@@ -244,44 +167,46 @@ def json_to_npy_no_trans(json_file_input):
                 bbox_min = map(int, (bbox_min * 100.0) / 6.0)
                 bbox_max = map(int, (bbox_max * 100.0) / 6.0)
 
-                for model in models:
-                    if str(model["id"]) == str_modelId:
-                        aligned_dims = model["aligned.dims"].split(",")
-
                 # transformation
-                object_voxel = trans_op(object_voxel, transformation, aligned_dims, bbox_min)
+                object_voxel = trans_op(object_voxel, transformation)
                 object_voxel = slice_non_zeroes(object_voxel)
-
-                # TODO: after transformation there is a lot of empty space in the object bbox, remove that spaces.
-                # TODO: how???
+                object_voxel = np.flip(object_voxel, 0)
 
                 # ==================================================
-                # output = open(str(str_modelId) + ".ply", 'w')
-                # ply = ""
-                # ver_num = 0
-                # for idx1 in range(object_voxel.shape[0]):
-                #     for idx2 in range(object_voxel.shape[1]):
-                #         for idx3 in range(object_voxel.shape[2]):
-                #             if object_voxel[idx1][idx2][idx3] >= 1:
-                #                 ply = ply + str(idx1) + " " + str(idx2) + " " + str(idx3) + " 0 128 0 255" + "\n"
-                #                 ver_num += 1
-                # output.write("ply" + "\n")
-                # output.write("format ascii 1.0" + "\n")
-                # output.write("comment VCGLIB generated" + "\n")
-                # output.write("element vertex " + str(ver_num) + "\n")
-                # output.write("property float x" + "\n")
-                # output.write("property float y" + "\n")
-                # output.write("property float z" + "\n")
-                # output.write("property uchar red" + "\n")
-                # output.write("property uchar green" + "\n")
-                # output.write("property uchar blue" + "\n")
-                # output.write("property uchar alpha" + "\n")
-                # output.write("element face 0" + "\n")
-                # output.write("property list uchar int vertex_indices" + "\n")
-                # output.write("end_header" + "\n")
-                # output.write(ply)
-                # output.close()
-                # print (str(str_modelId) + ".ply is Done.!")
+                output = open(str(str_modelId) + ".ply", 'w')
+                ply = ""
+                ver_num = 0
+                for idx1 in range(object_voxel.shape[0]):
+                    for idx2 in range(object_voxel.shape[1]):
+                        for idx3 in range(object_voxel.shape[2]):
+                            if object_voxel[idx1][idx2][idx3] >= 1:
+                                ply = ply + str(idx1) + " " + str(idx2) + " " + str(idx3) + " 0 128 0 255" + "\n"
+                                ver_num += 1
+                output.write("ply" + "\n")
+                output.write("format ascii 1.0" + "\n")
+                output.write("comment VCGLIB generated" + "\n")
+                output.write("element vertex " + str(ver_num+8) + "\n")
+                output.write("property float x" + "\n")
+                output.write("property float y" + "\n")
+                output.write("property float z" + "\n")
+                output.write("property uchar red" + "\n")
+                output.write("property uchar green" + "\n")
+                output.write("property uchar blue" + "\n")
+                output.write("property uchar alpha" + "\n")
+                output.write("element face 0" + "\n")
+                output.write("property list uchar int vertex_indices" + "\n")
+                output.write("end_header" + "\n")
+                output.write(ply)
+                output.write("0 0 0 0 128 0 255 \n")
+                output.write("0 0 "+str(object_voxel.shape[2])+" 0 128 0 255 \n")
+                output.write("0 "+str(object_voxel.shape[1])+" 0 0 128 0 255 \n")
+                output.write(str(object_voxel.shape[0])+" 0 0 0 128 0 255 \n")
+                output.write(str(object_voxel.shape[0])+" " +str(object_voxel.shape[1])+" "+str(object_voxel.shape[2])+" 0 128 0 255 \n")
+                output.write("0 " +str(object_voxel.shape[1])+" "+str(object_voxel.shape[2])+" 0 128 0 255 \n")
+                output.write(str(object_voxel.shape[0])+" 0 "+ str(object_voxel.shape[2])+" 0 128 0 255 \n")
+                output.write(str(object_voxel.shape[0])+" " +str(object_voxel.shape[1])+" 0 0 128 0 255 \n")
+                output.close()
+                print (str(str_modelId) + ".ply is Done.!")
                 # ==================================================
 
                 # put object_voxel into scene where object_voxel = True
@@ -339,25 +264,28 @@ def npy_to_ply(input_npy_file):
 
 def slice_non_zeroes(input_np):
     ones = np.argwhere(input_np)
+    # if ones.size == 0:
+    #     debugger = 1
+    #     return input_np
     (x_start, y_start, z_start), (x_stop, y_stop, z_stop) = ones.min(0), ones.max(0) + 1
     return input_np[x_start:x_stop, y_start:y_stop, z_start:z_stop]
 
-# ----------------------------------------------------------------------------------
 
+# ----------------------------------------------------------------------------------
 
 if __name__ == '__main__':
 
     # json to json s
-    if build_json_to_jsons:
-        for json_file in glob.glob('*.json'):
-            json_reader(json_file)
-            os.remove(json_file)
+    # if build_json_to_jsons:
+    #     for json_file in glob.glob('*.json'):
+    #         json_reader(json_file)
+    #         os.remove(json_file)
 
     # json to npy
     csv_loader()
     for json_file in glob.glob('*.json'):
         print (str(json_file))
-        json_to_npy_no_trans(json_file)
+        json_to_npy(json_file)
         # os.remove(json_file)
 
     # TODO: give label to each voxel
